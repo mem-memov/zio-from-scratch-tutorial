634a11d18f2d7867cc08117f606cb8c9ff206461
Alexander Novikov <mem-memov@yandex.ru>
2022-07-02 14:45:53 +0300

    up to async

--- src/main/scala/zio/Succeed.scala
+++ src/main/scala/zio/Succeed.scala
@@ -3,16 +3,81 @@ package zio
 case class Person(name: String, age: Int)
 
 object Person:
-  val peter = Person("Peter", 88)
+  val peter = Person("Peter", 48)
 
 trait ZIOApp:
   def run: ZIO[Any]
   def main(args: Array[String]): Unit =
     run.run { result =>
-      println(s"THE RESULT WAS $result}")
+      println(s"THE RESULT WAS ${result}")
     }
 
 
 object SucceedNow extends ZIOApp:
   val peterZIO: ZIO[Person] = ZIO.succeedNow(Person.peter)
   override def run: ZIO[Person] = peterZIO
+
+
+object SucceedNowUhOh extends ZIOApp:
+  val howdyZIO = ZIO.succeedNow(println("Howdy!"))
+  override def run: ZIO[Any] = ZIO.succeedNow(12)
+
+object Succees extends ZIOApp:
+  val howdyZIO = ZIO.succeed(println("Howdy!"))
+  override def run: ZIO[Any] = ZIO.succeedNow(23)
+
+object SucceedAgain extends ZIOApp:
+  def printLine(message: String): ZIO[Unit] =
+    ZIO.succeed(println(message))
+  override def run: ZIO[Any] = printLine("Fancy")
+
+object Zip extends ZIOApp:
+  val zippedZIO: ZIO[(Int, String)] = ZIO.succeed(8) zip ZIO.succeed("Lo")
+  override def run: ZIO[Any] = zippedZIO
+
+object Map extends ZIOApp:
+  val zippedZIO = ZIO.succeed(8) zip ZIO.succeed("LO")
+  val mappedZIO = zippedZIO.map { case (int, string) =>
+    string  * int
+  }
+  val personZIO = zippedZIO.map { case (age, name) => 
+    Person(name, age)
+  }
+  override def run: ZIO[Any] = personZIO
+
+object MapUhOh extends ZIOApp:
+  val zippedZIO = ZIO.succeed(8) zip ZIO.succeed("LO")
+  def printLine(message: String): ZIO[Unit] =
+    ZIO.succeed(println(message))
+  val mappedZIO = zippedZIO.map { tuple =>
+    printLine(s"MY BEAUTIFUL TUPLE: $tuple")
+  }
+  override def run: ZIO[Any] = mappedZIO
+
+object FlatMap extends ZIOApp:
+  val zippedZIO = ZIO.succeed(8) zip ZIO.succeed("LO")
+  def printLine(message: String): ZIO[Unit] =
+    ZIO.succeed(println(message))
+  val mappedZIO = zippedZIO.flatMap { tuple =>
+    printLine(s"MY BEAUTIFUL TUPLE: $tuple") //.as("Nice")
+  }
+  override def run: ZIO[Any] = mappedZIO
+
+object ForComprehension extends ZIOApp:
+  val zippedZIO = ZIO.succeed(8) zip ZIO.succeed("LO")
+  def printLine(message: String): ZIO[Unit] =
+    ZIO.succeed(println(message))
+  val flatmappedZIO = 
+    for {
+      tuple <- zippedZIO
+      _ <- printLine(s"MY BEAUTIFUL TUPLE: $tuple")
+    } yield "Nice"
+  override def run: ZIO[Any] = flatmappedZIO
+
+object Async extends ZIOApp:
+  val asyncZIO: ZIO[Int] = ZIO.async[Int] { complete =>
+    println("ASYNC BEGINNETH!")
+    Tread.sleep(2000)
+    complete(10)
+  }
+  override def run: ZIO[Any] = asyncZIO
--- src/main/scala/zio/ZIO.scala
+++ src/main/scala/zio/ZIO.scala
@@ -1,13 +1,44 @@
 package zio
 
-trait ZIO[+A]:
-  def run(callback: A => Unit): Unit
+sealed trait ZIO[+A]:
+  self =>
+    def run(callback: A => Unit): Unit
 
+    def as[B](value: => B): ZIO[B] =
+      map(_ => value)
+
+    def zip[B](that: ZIO[B]): ZIO[(A, B)] = 
+      for {
+        a <- self
+        b <- that
+      } yield (a, b)
+
+    def map[B](f: A => B): ZIO[B] = 
+      flatMap(a => ZIO.succeedNow(f(a)))
+
+    def flatMap[B](f: A => ZIO[B]): ZIO[B] =
+      ZIO.FlatMap(self, f)
 
 object ZIO:
 
-  def succeedNow[A](value: A): ZIO[A] =
-    new ZIO[A]:
-      override def run(callback: A => Unit): Unit =
-        callback(value)
+  def succeedNow[A](value: A): ZIO[A] = Succeed(value)
+
+  def succeed[A](value: => A): ZIO[A] = Effect(() => value)
+
+  case class Succeed[A](value: A) extends ZIO[A]:
+    override def run(callback: A => Unit): Unit =
+      callback(value)
+
+  case class Effect[A](f: () => A) extends ZIO[A]:
+    override def run(callback: A => Unit): Unit = 
+      callback(f())
+
+  case class FlatMap[A, B](zio: ZIO[A], f: A => ZIO[B]) extends ZIO[B]:
+    override def run(callback: B => Unit): Unit = 
+      zio.run { a =>
+        f(a).run(callback)
+      }
+
+
+      
 
